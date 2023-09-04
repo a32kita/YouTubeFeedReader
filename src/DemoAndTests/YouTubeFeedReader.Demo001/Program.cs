668152b2129a2562136ed68261d8cf768aa0eb5d
a32kita <a32kita@yahoo.co.jp>
2023-09-05 01:40:10 +0900

    wip

--- src/DemoAndTests/YouTubeFeedReader.Demo001/Program.cs
+++ src/DemoAndTests/YouTubeFeedReader.Demo001/Program.cs
@@ -4,12 +4,15 @@
     {
         static async Task Main(string[] args)
         {
-            var sourceUrl = new Uri("https://www.youtube.com/feeds/videos.xml?channel_id=UCP5xQ9HIcget8kMHHqgyP0Q");
+            var sourceUrl = new Uri("https://www.youtube.com/feeds/videos.xml?channel_id=UCw4J-5X7x1xUdbIcQkZ4VYA");
 
-            var ytService = new YouTubeFeedService();
-            var result = await ytService.LoadDocumentAsync(sourceUrl);
+            using (var ytService = new YouTubeFeedService())
+            {
+                var result = await ytService.LoadDocumentAsync(sourceUrl);
 
-            Console.WriteLine(result.Title);
+                // 下の行にブレークポイントを設置して、デバッガで値の正常性を確認する
+                Console.WriteLine(result.Title);
+            }
         }
     }
 }
